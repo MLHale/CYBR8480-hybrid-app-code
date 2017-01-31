9b094d1e99063e00129b0ec85391af374ef03f75
Matt Hale <mlhale@unomaha.edu>
2017-01-30 23:54:33 -0600

    added accelerometer display capability

---
+++ app/components/accelerometer-display.js
@@ -0,0 +1,39 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  x: 0,
+  y: 0,
+  on: true,
+  startLogging: function(){
+    //begin logging accelerometer data once the component launches
+
+    var component = this;
+    this.updateAccelData(component)
+    
+  }.on('init'),
+  updateAccelData: function(component){
+    Ember.run.later(function(){
+      //wrapper to preserve binding satistfaction
+      try {
+        //invoke cordova accelerometer Plugin and get accelerometer data
+        navigator.accelerometer.getCurrentAcceleration(function (acceleration) {//success callback
+            console.log('acceleration setvars called');
+            component.set('x', acceleration.x);
+            component.set('y', acceleration.y);
+            component.set('z', acceleration.z);
+            console.log("accel vals: x: "+ acceleration.x+ " y: "+acceleration.y+" z: "+acceleration.z+" t: "+ Date.now());
+        }, function (error) {//error callback
+            console.log('error: ' + error);
+        });
+      }
+      catch(err){
+        console.log('error: '+err);
+      }
+      if(component.get('on')){
+        //keep running
+        component.updateAccelData(component); //recurse
+      }
+
+    }, 100);//run ever 100ms
+  }
+});
