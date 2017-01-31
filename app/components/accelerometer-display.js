63a8dd9306e37dd805a19f7f0f14539951d7330e
Matt Hale <mlhale@unomaha.edu>
2017-01-31 01:41:44 -0600

    added ember-charts extension to display historical data

--- app/components/accelerometer-display.js
+++ app/components/accelerometer-display.js
@@ -4,6 +4,7 @@ export default Ember.Component.extend({
   x: 0,
   y: 0,
   z: 0,
+  accelHistory: [],
   on: true,
   startLogging: function(){
     //begin logging accelerometer data once the component launches
@@ -18,11 +19,24 @@ export default Ember.Component.extend({
       try {
         //invoke cordova accelerometer Plugin and get accelerometer data
         navigator.accelerometer.getCurrentAcceleration(function (acceleration) {//success callback
-            console.log('acceleration setvars called');
+            //console.log('acceleration setvars called');
             component.set('x', acceleration.x);
             component.set('y', acceleration.y);
             component.set('z', acceleration.z);
-            console.log("accel vals: x: "+ acceleration.x+ " y: "+acceleration.y+" z: "+acceleration.z+" t: "+ Date.now());
+
+            //update history, maintain 50 points max
+            var history=component.get('accelHistory');
+            if(history.length === 150){
+              history.shiftObject();//shift an x off
+              history.shiftObject();//shift a y off
+              history.shiftObject();//shift a z off
+            }
+            var t = Date.now();
+            var newXPoint = {time: t, label: 'x', value: acceleration.x};
+            var newYPoint = {time: t, label: 'y', value: acceleration.y};
+            var newZPoint = {time: t, label: 'z', value: acceleration.z};
+            history.addObjects([newXPoint, newYPoint, newZPoint]);
+            //console.log("accel vals: x: "+ acceleration.x+ " y: "+acceleration.y+" z: "+acceleration.z+" t: "+ Date.now());
         }, function (error) {//error callback
             console.log('error: ' + error);
         });
